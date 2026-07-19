b18c23d9248ac3b5913c3d2d831584bf2b9c9553
Paul Hoffmann <paulhoffmann410@gmail.com>
2026-07-19 16:15:21 +0200

    Add info command with license details in myline.py

--- myline.py
+++ myline.py
@@ -49,6 +49,7 @@ def Wprint(string):
 def WWprint(string):
     print(f"\033[0;47;30m{_prefix()} {string}\033[0m")
 
+Wprint("-" * 60)
 Wprint("Started MyLine...")
 Wprint("")
 
@@ -299,8 +300,18 @@ while True:
                 YYprint("net pg {url} {port}")
                 YYprint("ble HEAD devs {raw} {loop}")
                 YYprint("myline help")
+                YYprint("myline info")
                 YYprint("myline check changes")
                 YYprint("myline kill {force}")
+            elif cmd[1] == "info":
+                Wprint("My Line")
+                Wprint("github.com/hoffmann-paul/myline")
+                Wprint("")
+                Wprint("MIT License")
+                Wprint("Copyright (c) 2026 Paul Hoffmann")
+                Wprint("Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:")
+                Wprint("The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.")
+                Wprint("THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.")
             elif cmd[1] == "check":
                 if cmd[2] == "changes":
                     with open('Datensätze/data.json', 'r') as file:
